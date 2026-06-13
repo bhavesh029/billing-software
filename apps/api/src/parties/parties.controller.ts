c8ef3e6090da22a57ded7d5e839845728b0a9e62
Bhavesh Chauhan <bhaveshchauhan@Bhaveshs-MacBook-Air-2.local>
2026-06-13 15:26:20 +0530

    configure the application for the swagger API docs

--- apps/api/src/parties/parties.controller.ts
+++ apps/api/src/parties/parties.controller.ts
@@ -8,12 +8,15 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { OrgMemberGuard } from '../auth/org-member.guard';
 import { PartiesService } from './parties.service';
 import { CreatePartyDto } from './dto/create-party.dto';
 import { UpdatePartyDto } from './dto/update-party.dto';
 
+@ApiTags('Clients/Parties')
+@ApiBearerAuth('JWT-auth')
 @Controller('organizations/:orgId/parties')
 @UseGuards(JwtAuthGuard, OrgMemberGuard)
 export class PartiesController {
